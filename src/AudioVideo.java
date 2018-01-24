ee39cbc7c8eda1e1ce6b8a6f9076754fe9b253fb
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-01-24 14:42:38 +0300

    Update AudioVideo.java

--- src/AudioVideo.java
+++ src/AudioVideo.java
@@ -1,4 +1,10 @@
 public class AudioVideo extends Document {
+    
+    AudioVideo(String title, List<String> authors){
+        this.title = title;
+        this.authors = authors;
+    }
+    
 
     int checkOut(Document doc){
         //2 weeks
