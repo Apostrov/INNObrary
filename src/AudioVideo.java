287e2586eab81041d2017c56e3b0cf81c1449505
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-01-24 14:22:56 +0300

    Update AudioVideo.java

--- src/AudioVideo.java
+++ src/AudioVideo.java
@@ -1,6 +1,15 @@
 public class AudioVideo extends Document {
-    // TODO: getter for all info like title, author and other
+
     int checkOut(Document doc){
         //2 weeks
     }
+    
+    public String getTitle(){
+        return "";
+    }
+
+    public List<String> getAuthors(){
+        return null;
+    }
+    
 }
