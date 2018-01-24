8698a8caaa0d90b7732ed7f3e37b2c66d0ca7896
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-01-24 14:36:24 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -4,6 +4,15 @@ public class Book extends Document {
     int edition;
     int edition_year;
     String publisher;
+    
+    Book(String title, List<String> authors, int edition, int edition_year, String publisher){
+        this.title = title;
+        this.authors = authors;
+        this.edition = edition;
+        this.edition_year = edition_year;
+        this.publisher = publisher;
+    }
+    
 
     void checkOut(Document doc){
 
