21a115203f41e17238220441ac6e573eeb03a287
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-01-24 14:15:53 +0300

    Update JournalArticle.java

--- src/JournalArticle.java
+++ src/JournalArticle.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class JournalArticle extends Document {
     String journal;
     String issue;
-    ArrayList <String> issue_editors;
+    List <String> issue_editors;
     String publication_date;
     // TODO: getter for all info like title, author and other
 
