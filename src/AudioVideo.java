409196e5e443e5581540a41c49b0b07e7a9e2bd1
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-02-04 20:08:07 +0300

    Update AudioVideo.java

--- src/AudioVideo.java
+++ src/AudioVideo.java
@@ -1,19 +1,26 @@
 public class AudioVideo extends Document {
     
+     /** Initialisation of the audio/video material.
+     *  @param title, authors. */
     AudioVideo(String title, List<String> authors){
         this.title = title;
         this.authors = authors;
     }
     
-
+     /** Returns number of days you can check out the audio/video material.
+     *  @param book. */
     int checkOut(Document doc){
         //2 weeks
     }
     
+     /** Getter for title of the audio/video material.
+     *  @param title. */
     public String getTitle(){
         return title;
     }
 
+     /** Getter for authors of the audio/video material.
+     *  @param authors. */
     public List<String> getAuthors(){
         return authors;
     }
