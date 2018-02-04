769ec5adb0e64157caa685d0c6bdd9271d34c339
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-02-04 20:31:07 +0300

    Update Document.java

--- src/Document.java
+++ src/Document.java
@@ -2,37 +2,38 @@ import java.util.ArrayList;
 
 public class Document {
 
-    private String title;
-    private String [] keywords;
-    private List<String> authors;
-    private int price;
-    private int copies;
+    private String title; // title of the document
+    private String [] keywords; // keywords of the document
+    private List<String> authors; // authors of the document
+    private int price; // price of some document
+    private int copies; // number of copies of some document
     // TODO: position of document in library
 
+    /** To delete the document*/
     public void delete(){
 
     }
 
+    /** To add the document*/
     public void add(){
 
     }
 
+    /** To modify the document*/
     public void modify(Document new_doc){
 
     }
 
+    /** Returns result of searching documents by author */
     public Document[] searchFor_byAuthor(){
         Document[] result;
-        return result;
+        return null;
     }
 
+    /** Returns result of searching documents by title */
     public Document[] searchFor_byTitle(){
         Document[] result;
-        return result;
-    }
-
-    public Document search(){
-
+        return null;
     }
 
     /** Returns number of days of overdue */
@@ -40,11 +41,12 @@ public class Document {
         return 0;
     }
 
-
+    /** Returns the value of the fine */
     public int overdueFine(){
         return checkOverdue() * 100;
     }
 
+    /** Makes one copy of the document */
     public void make_copy(){
         copies++;
     }
