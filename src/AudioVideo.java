b3bd95b01c22c08194f83673a35beb4726554641
Aleksandra9999 <35536476+Aleksandra9999@users.noreply.github.com>
2018-02-05 18:19:47 +0300

    Update AudioVideo.java

--- src/AudioVideo.java
+++ src/AudioVideo.java
@@ -1,27 +1,29 @@
 import java.util.ArrayList;
 
 public class AudioVideo extends Document {
-    
-     /** Initialisation of the audio/video material.
-     *  @param title, authors. */
-    AudioVideo(String title, ArrayList<String> authors){
+
+    /** Initialisation of the audio/video material.
+     *  @param title, authors, price, copies. */
+    AudioVideo(String title, ArrayList<String> authors, int price, int copies){
         this.title = title;
         this.authors = authors;
+        this.price = price;
+        this.copies = copies;
     }
-    
-     /** Returns number of days you can check out the audio/video material.*/
+
+    /** Returns number of days you can check out the audio/video material.*/
     void checkOut(AudioVideo audiovideo){
         //2 weeks
     }
-    
-     /** Getter for title of the audio/video material.*/
+
+    /** Getter for title of the audio/video material.*/
     public String getTitle(){
         return title;
     }
 
-     /** Getter for authors of the audio/video material.*/
+    /** Getter for authors of the audio/video material.*/
     public ArrayList<String> getAuthors(){
         return authors;
     }
-    
+
 }
